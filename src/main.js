099ba1090e4871e5de7697c9c90fb95baad12d6e
Agung Jayasukma Prasetiyo <jsp.agung@gmail.com>
2024-03-24 15:01:22 +0700

    Add setupVisionHandlers to main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { setupAuthHandlers } from '../handlers/authHandlers.js';
 import { setupMessageHandlers } from '../handlers/messageHandlers.js';
+import { setupVisionHandlers } from '../handlers/visionHandlers.js';
 import initializeClient from './../clients/whatsappClient.js';
 
 import dotenv from 'dotenv';
@@ -9,5 +10,6 @@ const client = initializeClient();
 
 setupAuthHandlers(client);
 setupMessageHandlers(client);
+setupVisionHandlers(client);
 
 client.initialize();
