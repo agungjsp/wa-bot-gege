c6740142dfba0fd1c112007c2ad012808127b9de
Agung Jayasukma Prasetiyo <jsp.agung@gmail.com>
2024-04-07 11:40:18 +0700

    Add headless puppeteer configuration for WhatsApp client

--- clients/whatsappClient.js
+++ clients/whatsappClient.js
@@ -5,6 +5,19 @@ const wwebVersion = '2.2407.3';
 
 const initializeClient = () => {
     const client = new Client({
+        puppeteer: {
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process', // <- this one doesn't works in Windows
+                '--disable-gpu',
+            ],
+        },
         authStrategy: new LocalAuth(),
         webVersionCache: {
             type: 'remote',
